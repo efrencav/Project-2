9ebe84d69dccaaf3ff1bf6be046dd4c799a7937e
Efren Cavazos <efren.cavazos@gmail.com>
2018-12-03 20:08:59 -0600

    updated routes

--- server.js
+++ server.js
@@ -29,9 +29,8 @@ app.use(express.static("public"));
 
 // Routes
 // =============================================================
-// require("./routes/post-api-routes.js")(app);
-// require("./routes/author-api-routes.js")(app);
-// require("./routes/html-routes.js")(app);
+require("./routes/admin.js")(app);
+require("./routes/shop.js")(app);
 
 // Syncing our sequelize models and then starting our Express app
 // =============================================================
