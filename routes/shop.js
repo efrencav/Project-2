c695d8ce1e770be0c35af953ea89f6d5b0fe2673
efrencav <efren@abstract-creative.com>
2018-12-05 21:26:14 -0600

    changes

--- routes/shop.js
+++ routes/shop.js
@@ -15,14 +15,15 @@ module.exports = function(app) {
   // Each of the below routes just handles the HTML page that the user gets sent to.
 
   // index route loads view.html
-  app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "/"));
+  app.get("/shop", function(req, res) {
+    // res.sendFile(path.join(__dirname, "/"));
+    res.render('index', {});
   });
 
   // cms route loads cms.html
-  app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "/index.html"));
-  });
+  // app.get("/", function(req, res) {
+  //   res.sendFile(path.join(__dirname, "/index.html"));
+  // });
 
   // blog route loads blog.html
   app.get("/contact", function(req, res) {
--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-
 // *****************************************************************************
 // Server.js - This file is the initial starting point for the Node/Express server.
 //
@@ -7,7 +6,6 @@
 // =============================================================
 dotenv = require("dotenv").config();
 const express = require("express");
-<<<<<<< HEAD
 const multer = require('multer');
 require('handlebars');
 // const path = require('path');
@@ -15,104 +13,109 @@ require('handlebars');
 
 
 // =============================================================
-=======
 // Requiring our models for syncing
 const db = require("./models");
 const path = require("path");
 const session = require("express-session");
 //Passport for Login
 const passport = require("passport");
->>>>>>> d496bcd53c990d99bcf8bdb4e2d082e2c1a77a78
 
 
 // Set The Storage Engine
 const storage = multer.diskStorage({
-    destination: './public/uploads/',
-    filename: function(req, file, cb){
-      cb(null,file.fieldname + '-' + Date.now() + path.extname(file.originalname));
-    }
-  });
-  
-  // Init Upload
-const upload = multer({
-    storage: storage,
-    limits:{fileSize: 1000000},
-    fileFilter: function(req, file, cb){
-      checkFileType(file, cb);
-    }
-  }).single('myImage'); 
-  
-  // Check File Type
-  function checkFileType(file, cb){
-    // Allowed ext
-    const filetypes = /jpeg|jpg|png|gif/;
-    // Check ext
-    const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
-    // Check mime
-    const mimetype = filetypes.test(file.mimetype);
-  
-    if(mimetype && extname){
-      return cb(null,true);
-    } else {
-      cb('Error: Images Only!');
-    }
+  destination: './public/uploads/',
+  filename: function (req, file, cb) {
+    cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
   }
+});
 
-  // =============================================================
+// Init Upload
+const upload = multer({
+  storage: storage,
+  limits: {
+    fileSize: 1000000
+  },
+  fileFilter: function (req, file, cb) {
+    checkFileType(file, cb);
+  }
+}).single('myImage');
 
-  
+// Check File Type
+function checkFileType(file, cb) {
+  // Allowed ext
+  const filetypes = /jpeg|jpg|png|gif/;
+  // Check ext
+  const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+  // Check mime
+  const mimetype = filetypes.test(file.mimetype);
+
+  if (mimetype && extname) {
+    return cb(null, true);
+  } else {
+    cb('Error: Images Only!');
+  }
+}
+
+// =============================================================
+
+
 // Sets up the Express App
 // =============================================================
 const app = express();
 const PORT = process.env.PORT || 8080;
 // Sets up the Express app to handle data parsing
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({
+  extended: true
+}));
 app.use(express.json());
 
 app.use(express.static(path.join(__dirname, "./public")));
 
-<<<<<<< HEAD
 
 
 
-=======
->>>>>>> d496bcd53c990d99bcf8bdb4e2d082e2c1a77a78
 // Set Handlebars.
 const exphbs = require("express-handlebars");
-app.engine("hbs", exphbs({ defaultLayout: "main", extname: ".hbs" }));
+app.engine("hbs", exphbs({
+  defaultLayout: "main",
+  extname: ".hbs"
+}));
 app.set("view engine", "hbs");
 //Initialize Passport
 app.use(
-    session({ secret: "keyboard cat", resave: true, saveUninitialized: true })
+  session({
+    secret: "keyboard cat",
+    resave: true,
+    saveUninitialized: true
+  })
 ); // session secret
 app.use(passport.initialize());
 app.use(passport.session());
 // Import routes and give the server access to them.
 const router = express.Router();
 //const storeController = require("./controllers/store_controller.js");
-require("./routes/routes.js")(app, passport);
+require("./routes/login.js")(app, passport);
 app.use(router);
 
 require("./passport/passport.js")(passport, db.User);
 
 // Routes
 // =============================================================
-// require("./routes/admin.js")(app);
-// require("./routes/shop.js")(app);
+require("./routes/admin.js")(app);
+require("./routes/shop.js")(app);
 
 // Syncing our sequelize models and then starting our Express app
 // =============================================================
-<<<<<<< HEAD
 
 
 app.post('/upload', (req, res) => {
   upload(req, res, (err) => {
-    if(err){
+    if (err) {
       res.render('index', {
         msg: err
       });
     } else {
-      if(req.file == undefined){
+      if (req.file == undefined) {
         res.render('index', {
           msg: 'Error: No File Selected!'
         });
@@ -129,11 +132,12 @@ app.post('/upload', (req, res) => {
 // =============================================================
 
 
-db.sequelize.sync({}).then(function() {
-=======
-db.sequelize.sync({ force: true }).then(function() {
->>>>>>> d496bcd53c990d99bcf8bdb4e2d082e2c1a77a78
-    app.listen(PORT, function() {
-        console.log("App listening on PORT " + PORT);
-    });
-});
+db.sequelize.sync({}).then(function () {
+      db.sequelize.sync({
+        force: true
+      }).then(function () {
+        app.listen(PORT, function () {
+          console.log("App listening on PORT " + PORT);
+        });
+      });
+    })
