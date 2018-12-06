95f1c4bca52e4f95b5e59f9d8463c96559339d80
btdrizzle <briandrake05@gmail.com>
2018-12-06 00:14:54 -0600

    little auth fixes and linting things

--- server.js
+++ server.js
@@ -4,10 +4,10 @@
 // ******************************************************************************
 // *** Dependencies
 // =============================================================
-dotenv = require("dotenv").config();
+require("dotenv").config();
 const express = require("express");
-const multer = require('multer');
-require('handlebars');
+const multer = require("multer");
+require("handlebars");
 // const path = require('path');
 
 
@@ -23,37 +23,37 @@ const passport = require("passport");
 
 // Set The Storage Engine
 const storage = multer.diskStorage({
-  destination: './public/uploads/',
-  filename: function (req, file, cb) {
-    cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
-  }
+	destination: "./public/uploads/",
+	filename: function (req, file, cb) {
+		cb(null, file.fieldname + "-" + Date.now() + path.extname(file.originalname));
+	}
 });
 
 // Init Upload
 const upload = multer({
-  storage: storage,
-  limits: {
-    fileSize: 1000000
-  },
-  fileFilter: function (req, file, cb) {
-    checkFileType(file, cb);
-  }
-}).single('myImage');
+	storage: storage,
+	limits: {
+		fileSize: 1000000
+	},
+	fileFilter: function (req, file, cb) {
+		checkFileType(file, cb);
+	}
+}).single("myImage");
 
 // Check File Type
 function checkFileType(file, cb) {
-  // Allowed ext
-  const filetypes = /jpeg|jpg|png|gif/;
-  // Check ext
-  const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
-  // Check mime
-  const mimetype = filetypes.test(file.mimetype);
+	// Allowed ext
+	const filetypes = /jpeg|jpg|png|gif/;
+	// Check ext
+	const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+	// Check mime
+	const mimetype = filetypes.test(file.mimetype);
 
-  if (mimetype && extname) {
-    return cb(null, true);
-  } else {
-    cb('Error: Images Only!');
-  }
+	if (mimetype && extname) {
+		return cb(null, true);
+	} else {
+		cb("Error: Images Only!");
+	}
 }
 
 // =============================================================
@@ -65,7 +65,7 @@ const app = express();
 const PORT = process.env.PORT || 8080;
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({
-  extended: true
+	extended: true
 }));
 app.use(express.json());
 
@@ -78,17 +78,17 @@ app.use(express.static(path.join(__dirname, "public")));
 // Set Handlebars.
 const exphbs = require("express-handlebars");
 app.engine("hbs", exphbs({
-  defaultLayout: "main",
-  extname: ".hbs"
+	defaultLayout: "main",
+	extname: ".hbs"
 }));
 app.set("view engine", "hbs");
 //Initialize Passport
 app.use(
-  session({
-    secret: "keyboard cat",
-    resave: true,
-    saveUninitialized: true
-  })
+	session({
+		secret: "keyboard cat",
+		resave: true,
+		saveUninitialized: true
+	})
 ); // session secret
 app.use(passport.initialize());
 app.use(passport.session());
@@ -109,33 +109,32 @@ require("./routes/shop.js")(app);
 // =============================================================
 
 
-app.post('/upload', (req, res) => {
-  upload(req, res, (err) => {
-    if (err) {
-      res.render('index', {
-        msg: err
-      });
-    } else {
-      if (req.file == undefined) {
-        res.render('index', {
-          msg: 'Error: No File Selected!'
-        });
-      } else {
-        res.render('index', {
-          msg: 'File Uploaded!',
-          file: `uploads/${req.file.filename}`
-        });
-      }
-    }
-  });
+app.post("/upload", (req, res) => {
+	upload(req, res, (err) => {
+		if (err) {
+			res.render("index", {
+				msg: err
+			});
+		} else {
+			if (req.file == undefined) {
+				res.render("index", {
+					msg: "Error: No File Selected!"
+				});
+			} else {
+				res.render("index", {
+					msg: "File Uploaded!",
+					file: `uploads/${req.file.filename}`
+				});
+			}
+		}
+	});
 });
 
 // =============================================================
 
 
 db.sequelize.sync({}).then(function () {
-
-    app.listen(PORT, function () {
-        console.log("App listening on PORT " + PORT);
-    });
+	app.listen(PORT, function () {
+		console.log("App listening on PORT " + PORT);
+	});
 });
