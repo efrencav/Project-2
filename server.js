07dd8f180fd36367687a33b5a0447627f451871e
efrencav <efren@abstract-creative.com>
2018-12-10 22:33:54 -0600

    new pull from develop

--- server.js
+++ server.js
@@ -23,7 +23,9 @@ const passport = require("passport");
 
 // Set The Storage Engine
 const storage = multer.diskStorage({
-	destination: "./public/uploads/",
+	destination: function(req, file, cb){
+		cb(null, "./public/uploads/");
+	}, 
 	filename: function (req, file, cb) {
 		cb(null, file.fieldname + "-" + Date.now() + path.extname(file.originalname));
 	}
